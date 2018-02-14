7ed634b658ea03b015c33a3e7c2b3ee2ab0e383e
seisyo58 <seisyo58@gmail.com>
2018-02-14 21:33:10 +0900

    hoge

---
+++ myGithub.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+git add .
+
+echo -n "input comment ->"
+read str
+if [ -n "$str" ]; then
+	git commit -m $str
+else
+	git commit -m $date
+fi
+
+dir=`pwd`
+$dir=`basename ${DIR}`
+
+git remote add origin https://github.com/seisyo58/"$dir".git
+
+git push origin master
